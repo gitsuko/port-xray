145c6c7d9644ae17861fd7aa46a6ded0e087b7bb
gitsuko <kishijung@gmail.com>
2025-08-06 10:48:13 +0330

    Update description

--- arguments.py
+++ arguments.py
@@ -3,7 +3,12 @@ import argparse
 def args(get_host, get_port):
     description = """
     This is simple Port Scanner app written in python.
-    Currect protocol supprting: TCP."""
+    Currect protocol supprting: TCP.
+    
+    Examples:
+        python3 main.py -a 192.168.10.10 -p 80-120
+        python3 main.py -a 192.168.10.10/24 -p 80
+        python3 main.py -a 192.168.10.10-20 -p 80"""
 
     parser = argparse.ArgumentParser(
         description=description,
