0efb6b58b6b470480dd0b82389b4de2135b3794c
gitsuko <kishijung@gmail.com>
2025-08-07 22:34:20 +0330

    Add default port

--- arguments.py
+++ arguments.py
@@ -6,8 +6,8 @@ def args(get_host, get_port):
     Currect protocol supprting: TCP.
     
     Examples:
-        python3 main.py -a 192.168.10.10 -p 80-120
-        python3 main.py -a 192.168.10.10/24 -p 80
+        python3 main.py -a 192.168.10.10 -p 22
+        python3 main.py -a 192.168.10.10/24 -p 80-24
         python3 main.py -a 192.168.10.10-20 -p 80"""
 
     parser = argparse.ArgumentParser(
@@ -23,7 +23,8 @@ def args(get_host, get_port):
     parser.add_argument(
         "-p", "--port",
         type=get_port,
-        help="Port(s) for scanning target. It should be digits only. (e.g. -p 443 or -p 80-443)"
+        help="Port(s) for scanning target. It should be digits only. (e.g. -p 443 or -p 80-443)",
+        default="1-1000"
     )
 
     return parser.parse_args()
