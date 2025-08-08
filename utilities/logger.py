7b2407bb3c2abc990ac09ced7cc3025e1968b0d1
gitsuko <kishijung@gmail.com>
2025-08-08 11:03:20 +0330

    Change stdout format

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,15 +1,16 @@
 import logging as lo
 
 def logging_function():
-    format = "%(asctime)s - %(levelname)s - %(message)s"
+    file_format = "%(asctime)s - %(levelname)s - %(message)s"
+    stdout_format = "%(asctime)s - %(message)s"
     root = lo.getLogger()
     root.setLevel(lo.INFO)
 
     fileHandler = lo.FileHandler("logger.txt")
-    fileHandler.setFormatter(lo.Formatter(format, datefmt="%Y-%M-%d %H:%M:%S"))
+    fileHandler.setFormatter(lo.Formatter(file_format, datefmt="%Y-%M-%d %H:%M:%S"))
 
     consoleHandler = lo.StreamHandler()
-    consoleHandler.setFormatter(lo.Formatter(format, datefmt="%Y-%m-%d %H:%M:%S"))
+    consoleHandler.setFormatter(lo.Formatter(stdout_format, datefmt="%H:%M:%S"))
 
     root.addHandler(fileHandler)
     root.addHandler(consoleHandler)
